89a5621add3898c1eeae7f6a3c4556bf10f36e41
Leevi Ojala <leevi.ojala@TTLT72.local>
2020-03-20 16:17:57 +0200

    added info

--- src/components/Count.js
+++ src/components/Count.js
@@ -2,6 +2,10 @@ import React, { Fragment } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+
+import InfoIcon from "@material-ui/icons/Info";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
 export default function Count(props) {
   console.log("count :" + props.count);
   return (
@@ -14,6 +18,11 @@ export default function Count(props) {
             <Typography>Arvio todellisesta tartuntojen määrästä:</Typography>
             <Typography variant="h4">
               {props.count * 10} - {props.count * 30}
+              <Tooltip title="Perustuu THL:n antamaan haastatteluun: https://yle.fi/uutiset/3-11259147">
+                <IconButton>
+                  <InfoIcon />
+                </IconButton>
+              </Tooltip>
             </Typography>
           </Fragment>
         ) : (
